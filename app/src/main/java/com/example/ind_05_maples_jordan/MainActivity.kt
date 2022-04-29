3736def1a52ab1bc71cd373f8f1272aafd94c851
Jordan Maples <jordan.d.maples@gmail.com>
2022-04-28 22:06:06 -0500

    Populated Name and Nickname

--- app/src/main/java/com/example/ind_05_maples_jordan/MainActivity.kt
+++ app/src/main/java/com/example/ind_05_maples_jordan/MainActivity.kt
@@ -2,41 +2,30 @@ package com.example.ind_05_maples_jordan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ind_05_maples_jordan.R.drawable.ic_launcher_background
+import com.example.ind_05_maples_jordan.State_Data.stateList
+import com.example.ind_05_maples_jordan.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), ViewAdapter.OnItemClickListener {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(500)
-
         val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerview.adapter = ViewAdapter(exampleList)
+        recyclerview.adapter = ViewAdapter(stateList, this)
         recyclerview.layoutManager = LinearLayoutManager(this)
         recyclerview.setHasFixedSize(true)
 
     }
 
-    private fun generateDummyList(size: Int): List<SampleItem> {
+    override fun onItemClick(position: Int) {
 
-        val list = ArrayList<SampleItem>()
-
-        for (i in 0 until size)
-        {
-            val drawable = when (i % 3) {
-                0 -> R.drawable.ic_baseline_flag_24
-                1 -> ic_launcher_background
-                else -> R.drawable.ic_launcher_foreground
-            }
-
-            val item = SampleItem(drawable, "Item $i", "Line 2")
-            list += item
-        }
-        return list
     }
 }
--- app/src/main/java/com/example/ind_05_maples_jordan/ViewAdapter.kt
+++ app/src/main/java/com/example/ind_05_maples_jordan/ViewAdapter.kt
@@ -1,20 +1,37 @@
 package com.example.ind_05_maples_jordan
 
-import android.graphics.Color
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class ViewAdapter(private val exampleList: List<SampleItem>) : RecyclerView.Adapter<ViewAdapter.ExampleViewHolder>() {
+class ViewAdapter(val stateList: List<State_Info>,
+                private val listener: OnItemClickListener
+                ) : RecyclerView.Adapter<ViewAdapter.ExampleViewHolder>() {
 
-    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.image_detail)
+    inner class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    View.OnClickListener{
+
         val textView1: TextView = itemView.findViewById(R.id.text_view_1)
         val textView2: TextView = itemView.findViewById(R.id.text_view_2)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val position = bindingAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
@@ -26,16 +43,16 @@ class ViewAdapter(private val exampleList: List<SampleItem>) : RecyclerView.Adap
 
     // Takes the current view holder and fills it with the data of the item in our current data set
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
-        val currentItem = exampleList[position]
+        val currentItem = stateList[position]
 
-        holder.imageView.setImageResource(currentItem.imageResource)
-        holder.textView1.text = currentItem.text1
-        holder.textView2.text = currentItem.text2
+        holder.textView1.text = currentItem.stateName
+        holder.textView2.text = currentItem.nickName
 
     }
 
     // Recycler View needs to know how many items are in our list
-    override fun getItemCount() = exampleList.size
-
+    override fun getItemCount(): Int {
+        return stateList.size
+    }
 
 }
