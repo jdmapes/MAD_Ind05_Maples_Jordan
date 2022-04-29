bc6a21c271e9fb614c5bcc97ca62a0c23179b5fc
Jordan Maples <jordan.d.maples@gmail.com>
2022-04-29 00:36:09 -0500

    build class objects

--- app/src/main/java/com/example/ind_05_maples_jordan/SeeDaFlag.kt
+++ app/src/main/java/com/example/ind_05_maples_jordan/SeeDaFlag.kt
@@ -2,10 +2,18 @@ package com.example.ind_05_maples_jordan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ind_05_maples_jordan.databinding.ActivityMainBinding
 
 class SeeDaFlag : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_see_da_flag)
     }
+
+
 }
--- app/src/main/java/com/example/ind_05_maples_jordan/State_Info.kt
+++ app/src/main/java/com/example/ind_05_maples_jordan/State_Info.kt
@@ -1,3 +1,3 @@
 package com.example.ind_05_maples_jordan
 
-data class State_Info(val stateName: String, val nickName: String, val stateArea: Int)
+data class State_Info(val stateName: String, val nickName: String, val stateArea: Int, val outline: String, val stateFlag: String)
